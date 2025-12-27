04c1412ab51e12fd8359867958c20e74bc76f5d4
Paul Lai <wclai.paul@gmail.com>
2025-12-27 16:16:41 +0800

    Add to render

--- main.py
+++ main.py
@@ -198,4 +198,5 @@ def handle_image(event):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
